4c6920c3b761af53792fe52eee6f79d37ac282de
pai_commits <sumer_cup@hotmail.com>
2022-04-19 17:00:04 +0700

    generate

---
+++ _nuxt/70f6330.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[80],{915:function(t,e,n){"use strict";n.r(e);var o={layout:"home"},l=n(57),component=Object(l.a)(o,(function(){var t=this.$createElement,e=this._self._c||t;return e("div",[e("Home-Contract")],1)}),[],!1,null,null,null);e.default=component.exports}}]);
--- plugins/city.js
+++
@@ -1,4 +0,0 @@
-import Vue from 'vue';
-import VueThailandAddress from 'vue-thailand-address';
-import 'vue-thailand-address/dist/vue-thailand-address.css';
-Vue.use(VueThailandAddress)
