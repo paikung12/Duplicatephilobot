578944fa98dad6450cec759137b0e19373a37d67
pai_commits <sumer_cup@hotmail.com>
2022-04-19 16:32:04 +0700

    del node

--- .nuxt/vuetify/options.js
+++
@@ -1,1 +0,0 @@
-export default {"theme":{"dark":false,"themes":{"light":{"primary":"#4ade80","accent":"#0d6efd","secondary":"#8A8D93","success":"#56CA00","info":"#16B1FF","warning":"#FFB400","error":"#FF4C51"},"dark":{"primary":"#1698f9","accent":"#0d6efd","secondary":"#8A8D93","success":"#56CA00","info":"#16B1FF","warning":"#FFB400","error":"#FF4C51"}}}}
